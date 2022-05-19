94aa822d2f95f39ba4c03fd068cbd83cfb895e71
Mayra Gutierrez <mayra.gutierrezlopez7@gmail.com>
2022-05-19 17:00:07 +0200

    vfcf

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+
+puts "Cleaning up database"
+Bookmark.delete_all
+Movie.delete_all
+List.delete_all
+puts 'Database is now clean'
+
+puts 'Parsing movies from tmdb.lewagon.com'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+json = URI.open(url).read
+content = JSON.parse(json)['results']
+poster_path = 'https://image.tmdb.org/t/p/w500'
+
+puts 'Creating movies'
+content.first(20).each do |movie|
+  poster_url_full = poster_path + movie['poster_path']
+  Movie.create!(title: movie['original_title'], overview: movie['overview'], poster_url: poster_url_full, rating: movie['vote_average'])
+  puts 'Movies created'
+end
+
+puts 'Done!'
